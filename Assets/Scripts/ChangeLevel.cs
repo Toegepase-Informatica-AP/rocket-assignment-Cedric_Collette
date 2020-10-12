67b2271b89d73f431d047f72e2d7ccfa757ba443
GarishDonkey <cedriccollette@live.be>
2020-10-12 11:02:11 +0200

    initial commit

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public string levelToLoad;
+    public void OnCollisionEnter(Collision col)
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
---
+++ Assets/Scripts/ShowWinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowWinner : MonoBehaviour
+{
+    public GameObject winnerCavas;
+
+    public void Start()
+    {
+        winnerCavas.SetActive(false);
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        winnerCavas.SetActive(true);
+    }
+}
